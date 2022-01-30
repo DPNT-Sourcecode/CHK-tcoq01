3825f7b4d3415b1841f5a66674411e8e3f20b7b6
sbx_user1051 <sbx_user1051@169.254.11.165>
2022-01-30 15:35:24 +0000

    Sun Jan 30 15:35:24 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,6 @@
 from collections import Counter, namedtuple
 
-Offer = namedtuple('Offer', ['price', 'quantity', 'free_item'])
+Offer = namedtuple('Offer', ['item', 'price', 'quantity', 'combined_item'])
 
 PRICE_LIST = {
     'A': 50,
@@ -10,11 +10,12 @@ PRICE_LIST = {
     'E': 40,
 }
 
-SPECIAL_OFFERS = {
-    'A': [Offer(200, 5, None), Offer(130, 3, None)],
-    'B': [Offer(45, 2, None)],
-    'E': [Offer(80, 2, 'B')],
-}
+ORDERED_SPECIAL_OFFERS = [
+    Offer('A', 200, 5, None),
+    Offer('A', 130, 3, None),
+    Offer('E', 80, 2, 'B'),
+    Offer('B', 45, 2, None),
+]
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -25,12 +26,27 @@ def checkout(skus):
     total = 0
     checkout_items = Counter(skus)
 
+    for sku, offers in ORDERED_SPECIAL_OFFERS.items():
+        if sku not in checkout_items:
+            continue
+
+        quantity = checkout_items[sku]
+
+        for offer in offers:
+            applied_offer_count = int(quantity / offer.quantity)
+
+            combined_item = offer.combined_item
+            if combined_item:
+                checkout_items[combined_item] -= applied_offer_count
+                total += offer.price * applied_offer_count
+
     for sku, quantity in checkout_items.items():
         if sku not in PRICE_LIST:
             return -1
 
         remaining = quantity
         item_price = PRICE_LIST[sku]
+
         if sku in SPECIAL_OFFERS:
             offers = SPECIAL_OFFERS[sku]
             for offer in offers:
@@ -46,3 +62,4 @@ def checkout(skus):
         total += item_price * remaining
     
     return total
+
