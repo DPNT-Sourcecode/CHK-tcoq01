fb02c8636d06580664fe1da1fc52409a2f6d8b7a
sbx_user1051 <sbx_user1051@169.254.11.165>
2022-01-30 14:55:21 +0000

    Sun Jan 30 14:55:21 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,20 @@
-from collections import Counter
+from collections import Counter, namedtuple
+
+ItemPrice = namedtuple(
+    'ItemPrice',
+    [
+        'price',
+        'special_offer_price',
+        'special_offer_quantity'
+    ],
+)
+
+PRICE_LIST = {
+    'A': ItemPrice(50, 130, 3),
+    'B': ItemPrice(30, 45, 2),
+    'C': ItemPrice(20),
+    'D': ItemPrice(15),
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -6,4 +22,3 @@ def checkout(skus):
     total = 0
     items = Counter(skus)
     
-
