b216d8efdb7a54b04ffd8b6884f0e13a7e2c45b3
sbx_user1051 <sbx_user1051@169.254.11.165>
2022-01-30 15:58:18 +0000

    Sun Jan 30 15:58:18 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,6 +9,27 @@ PRICE_LIST = {
     'D': 15,
     'E': 40,
     'F': 10,
+    'G': 20,
+    'H': 10,
+    'I': 35,
+    'J': 60,
+    'K': 80,
+    'L': 90,
+    'M': 15,
+    'N': 40,
+    'O': 10,
+    'P': 50,
+    'Q': 30,
+    'R': 50,
+    'S': 30,
+    'T': 20,
+    'U': 40,
+    'V': 50,
+    'W': 20,
+    'X': 90,
+    'Y': 10,
+    'Z': 50,
+
 }
 
 ORDERED_SPECIAL_OFFERS = [
@@ -17,6 +38,16 @@ ORDERED_SPECIAL_OFFERS = [
     Offer('E', 80, 2, 'B'),
     Offer('B', 45, 2, None),
     Offer('F', 20, 2, 'F'),
+    Offer('H', 45, 5, None),
+    Offer('H', 80, 10, None),
+    Offer('K', 150, 2, None),
+    Offer('N', 120, 3, 'M'),
+    Offer('P', 200, 5, None),
+    Offer('R', 150, 3, 'Q'),
+    Offer('Q', 80, 3, None),
+    Offer('U', 120, 3, 'U'),
+    Offer('V', 130, 3, None),
+    Offer('V', 90, 2, None),
 ]
 
 
@@ -51,5 +82,6 @@ def checkout(skus):
         total += PRICE_LIST[sku] * quantity
     
     return total
+
 
 
